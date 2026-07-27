b063b93ebfb42566ca0827d2bbb60d2b22cb8261
JoaoCanhadas <113210665+JoaoCanhadas@users.noreply.github.com>
2026-07-27 14:36:11 -0300

    Dados atualizados em 27/07/2026 14:35

--- manter_sincronizacao_tempo_real.bat
+++ manter_sincronizacao_tempo_real.bat
@@ -1,3 +1,6 @@
 @echo off
 cd /d "%~dp0"
+if exist ".env" (
+  for /f "usebackq tokens=1,* delims==" %%A in (".env") do set "%%A=%%B"
+)
 python sincronizar_sql_online.py --interval 60 >> sincronizacao_sql_online.log 2>&1
