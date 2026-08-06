86f180c695e446490ec509ceed6e20531e0bb1fe
JoaoCanhadas <113210665+JoaoCanhadas@users.noreply.github.com>
2026-08-06 10:28:04 -0300

    Dados atualizados em 06/08/2026 10:27

--- dashboard.py
+++ dashboard.py
@@ -1093,7 +1093,7 @@ function changeToNextView() {
 
 function startAutoRotate() {
   if (autoRotateTimer) clearInterval(autoRotateTimer);
-  autoRotateTimer = setInterval(changeToNextView, 50000);
+  autoRotateTimer = setInterval(changeToNextView, 60000);
 }
 
 loadData();
