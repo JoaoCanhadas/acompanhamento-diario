f8b8d4c35060d182ed0531dc6f89f795b42553e8
JoaoCanhadas <113210665+JoaoCanhadas@users.noreply.github.com>
2026-09-10 13:37:32 -0300

    Dados atualizados em 10/09/2026 13:36

--- dashboard.py
+++ dashboard.py
@@ -2242,7 +2242,7 @@ function startAutoRotate() {
 
 loadData();
 setInterval(loadData, 30000);
-startAutoRotate();
+// startAutoRotate(); // transição automática temporariamente
   </script>
 </body>
 </html>
